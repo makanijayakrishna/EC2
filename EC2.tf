87f193547fc130ccfb9063724cd1721bb0e4097a
makanijayakrishna <jayakrishnamakani@gmail.com>
2022-06-28 10:05:13 +0530

    adding instance

---
+++ EC2.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "Terraformtest1" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  key_name      = "AWS Terraformkeypair"
+
+  tags = {
+    Name = "testing 1994"
+   
+  }
+}
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1c"
+  size              = "8"
+  type              = "gp2"
+
+  tags = {
+    Name = "usuage"
+  }
+}
